baecdcbd50b4d04652c8d750fff6339f70a2987c
0xJAF <jeanandrewfuentes@gmail.com>
2024-12-24 18:17:33 +0800

    Create Get-Sysmon.psm1

---
+++ soc/Get-Sysmon.psm1
@@ -0,0 +1,19 @@
+function Get-SysmonEvent{
+    param (
+        $eventid,
+        $start,
+        $end
+    )
+    $filters = @{LogName = "Microsoft-Windows-Sysmon/Operational"}
+    if ($eventid -ne $null) {
+        $filters.ID = $eventid
+    }
+    if ($start -ne $null) {
+        $filters.StartTime = $start
+    }
+
+    if ($end -ne $null) {
+        $filters.EndTime = $end
+    }
+    Get-WinEvent -FilterHashtable $filters
+}
